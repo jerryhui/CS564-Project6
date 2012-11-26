f09d2a59efdaadadcd2ae02d00066e9756e43988
Jerry Hui <chiwei.hui@gmail.com>
2012-11-26 17:01:42 -0600

    QU_Delete first implementation

--- delete.C
+++ delete.C
@@ -8,19 +8,36 @@
  * Returns:
  * 	OK on success
  * 	an error code otherwise
+ *
+ * Log:
+ * 2012/11/26 JH: First implementation.
  */
-
 const Status QU_Delete(const string & relation, 
 		       const string & attrName, 
 		       const Operator op,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
-
-
-
+    // rejects when no relation or attribute is given
+    if (relation.empty() || attrName.empty()) return BADCATPARM;
+    
+    Status status;
+    RID rid;
+    HeapFileScan *hfs = new HeapFileScan(relation, status);
+    if (status!=OK) return status;
+    
+    hfs->startScan(0, 0, type, attrValue, op);
+    // if error occurs during startScan, the loop below is skipped and exit steps are taken
+    
+    // search for records that match given condition
+    while (status==OK) {
+        status = hfs->scanNext(rid);
+        if (status == FILEEOF) break;
+        
+        hfs->deleteRecord();
+    }
+    
+    // delete record
+    delete hfs;
+    return OK;
 }
-
-

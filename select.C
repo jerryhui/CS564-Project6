a70a2fd4e4247ce1a66267805cc9cf044f8d54e7
Jerry Hui <chiwei.hui@gmail.com>
2012-11-30 16:05:13 -0600

    QU_Select debug
    Need to check if attr is NULL

--- select.C
+++ select.C
@@ -34,16 +34,18 @@ const Status QU_Select(const string & result,
     // Qu_Select sets up things and then calls ScanSelect to do the actual work
     cout << "Doing QU_Select " << endl;
 
-    // reject invalid format
-    if (attr->attrType <0 || attr->attrType>2) return BADCATPARM;
-    
     Status status;
     AttrDesc *attrDesc;
     AttrDesc projAttrDesc[projCnt];
     int attrCnt,i,j,reclen,searchAttr;
     
+    // reject invalid format
+    if ( (attr != NULL)
+        && (attr->attrType <0 || attr->attrType>2) )
+        return BADCATPARM;
+
     // get info of all attributes in requested relation
-    if ( (status=attrCat->getRelInfo(attr->relName, attrCnt, attrDesc)) != OK)
+    if ( (status=attrCat->getRelInfo(projNames[0].relName, attrCnt, attrDesc)) != OK)
         return status;
     
     reclen=0;
@@ -56,13 +58,14 @@ const Status QU_Select(const string & result,
                 reclen += attrDesc[i].attrLen;
                 projAttrDesc[j] = attrDesc[i];
             }
-        if (strcmp(attrDesc[i].attrName,attr->attrName) == 0)
+        if (attr!=NULL && strcmp(attrDesc[i].attrName,attr->attrName) == 0)
             searchAttr=i;
     }
     
     // pass info along to ScanSelect for the actual work
     return ScanSelect(result, projCnt, projAttrDesc,
-                      &attrDesc[searchAttr], op, attrValue, reclen);
+                      &attrDesc[searchAttr],
+                      op, attrValue, reclen);
 }
 
 
@@ -91,18 +94,24 @@ const Status ScanSelect(const string & result,
     // open relation file to read, result file to write
     InsertFileScan resultFile(result,status);
     if (status!=OK) return status;
-    HeapFileScan relFile(attrDesc->relName,status);
+    HeapFileScan relFile(projNames[0].relName,status);
     if (status!=OK) return status;
-    
+
     // set up scan condition
-    switch (attrDesc->attrType) {
-        case 0: t = INTEGER; break;
-        case 1: t = FLOAT; break;
-        case 2: t = STRING; break;
+    if (attrDesc!=NULL) {
+        switch (attrDesc->attrType) {
+            case 0: t = INTEGER; break;
+            case 1: t = FLOAT; break;
+            case 2: t = STRING; break;
+        }
+        status=relFile.startScan(attrDesc->attrOffset,
+                                 attrDesc->attrLen,
+                                 t, filter, op);
+    } else {
+        // no condition is given, do an unconditional scan
+        status=relFile.startScan(0,0,STRING, NULL, EQ);
     }
-    status=relFile.startScan(attrDesc->attrOffset,
-                             attrDesc->attrLen,
-                             t, filter, op);
+        
     if  ( status != OK ) return status;
     
     while (relFile.scanNext(rid) == OK) {
